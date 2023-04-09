aad096a07ef1b51ca0dc620de40f8b6ced0f4587
moonkorea00 <moonkorea00@gmail.com>
2023-04-09 18:08:25 +0900

    fix : slider asset paths

--- src/components/Slider/slider.utils.ts
+++ src/components/Slider/slider.utils.ts
@@ -1,12 +1,14 @@
+import { assetPaths } from '@utils/assets';
+
 export const SLIDER_IMG_DATA = [
-  { id: 1, src: '/slider/haduri2.png', title: '서울 간지남(21)' },
-  { id: 2, src: '/slider/haduri1.png', title: '밤새고 찰칵 >.<' },
-  { id: 3, src: '/slider/haduri3.png', title: '뭐' },
-  { id: 5, src: '/slider/haduri5.png', title: '셀카ㅋ^^' },
-  { id: 6, src: '/slider/haduri6.png', title: '도봉중 1짱' },
-  { id: 7, src: '/slider/haduri7.png', title: '휴가' },
-  { id: 8, src: '/slider/haduri8.png', title: '큐티샷ㅋ' },
-  { id: 9, src: '/slider/haduri9.png', title: '지켜줄게' },
-  { id: 10, src: '/slider/haduri10.png', title: '이쁘니^^' },
-  { id: 11, src: '/slider/haduri11.png', title: '연락해' },
+  { id: 1, src: assetPaths.slider.haduri1, title: '서울 간지남(21)' },
+  { id: 3, src: assetPaths.slider.haduri2, title: '뭐' },
+  { id: 2, src: assetPaths.slider.haduri3, title: '밤새고 찰칵 >.<' },
+  { id: 5, src: assetPaths.slider.haduri4, title: '셀카ㅋ^^' },
+  { id: 6, src: assetPaths.slider.haduri5, title: '도봉중 1짱' },
+  { id: 7, src: assetPaths.slider.haduri6, title: '휴가' },
+  { id: 8, src: assetPaths.slider.haduri7, title: '큐티샷ㅋ' },
+  { id: 9, src: assetPaths.slider.haduri8, title: '지켜줄게' },
+  { id: 10, src: assetPaths.slider.haduri9, title: '이쁘니^^' },
+  { id: 11, src: assetPaths.slider.haduri10, title: '연락해' },
 ];
--- src/utils/assets.ts
+++ src/utils/assets.ts
@@ -1,6 +1,18 @@
 export const assetPaths = {
   watermark: '/assets/haduri.svg',
   uploadImg: '/assets/upload/upload.png',
+  slider: {
+    haduri1: '/assets/slider/haduri2.png',
+    haduri2: '/assets/slider/haduri3.png',
+    haduri3: '/assets/slider/haduri1.png',
+    haduri4: '/assets/slider/haduri5.png',
+    haduri5: '/assets/slider/haduri6.png',
+    haduri6: '/assets/slider/haduri7.png',
+    haduri7: '/assets/slider/haduri8.png',
+    haduri8: '/assets/slider/haduri9.png',
+    haduri9: '/assets/slider/haduri10.png',
+    haduri10: '/assets/slider/haduri11.png',
+  },
   favicon: '/assets/favicon/haduri.ico',
   appleTouchIcon: '/assets/favicon/haduri.png',
   ogImage: '/assets/haduri-zzal-bang.png',
